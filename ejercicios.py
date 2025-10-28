742146f14919424daaa756834382c4ab25eda22e
20255916 <20255916@esen.edu.sv>
2025-10-28 17:08:34 -0600

    nh

--- ejercicios.py
+++ ejercicios.py
@@ -1,2 +1,18 @@
 """nada"""
 
+palabra= "hola"
+#len(palabra) --- para decir la longitud
+
+#print(len(palabra))
+
+lista = [10, 11, 12, 13, 14]
+#print(len(lista))
+
+dias= ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+#print(len(dias))
+
+for i in palabra:        #i toma la posicion de cada uno de los elementos, por eje. en palabra es 4 veces.
+    print("hola profe")  #en lista 5 veces y en dias 7 veces porque repite el print dependiendo de cuantos elementos
+                         #hay en la lista
+                         
+    
