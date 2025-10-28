c7e93e8cb4b1b95f3809f256cf0856460009bf99
20255916 <20255916@esen.edu.sv>
2025-10-28 17:27:00 -0600

    mjgnk

--- ejercicios.py
+++ ejercicios.py
@@ -14,5 +14,9 @@ dias= ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo
 for i in palabra:        #i toma la posicion de cada uno de los elementos, por eje. en palabra es 4 veces.
     print("hola profe")  #en lista 5 veces y en dias 7 veces porque repite el print dependiendo de cuantos elementos
                          #hay en la lista
-                         
+
+#no entiendo nada
+#ayuda
+#ayuda
+
     
